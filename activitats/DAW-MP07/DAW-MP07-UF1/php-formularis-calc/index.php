f11f721e398a8c47005d821405722c58374da79e
Aniol Lidon <alidon1@xtec.cat>
2022-09-27 13:50:35 +0200

    Afegint php-formularis-calc

---
+++ activitats/DAW-MP07/DAW-MP07-UF1/php-formularis-calc/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+
+    <title>Calculadora</title>
+</head>
+<body>
+    <div class="container">
+        <form name="calc" class="calculator">
+            <input type="text" class="value" readonly value="" />
+            <span class="num clear"><input type ="submit" value="C"></span>
+            <span class="num"><input type ="submit" value="/"></span>
+            <span class="num"><input type ="submit" value="*"></span>
+            <span class="num"><input type ="submit" value="7"></span>
+            <span class="num"><input type ="submit" value="8"></span>
+            <span class="num"><input type ="submit" value="9"></span>
+            <span class="num"><input type ="submit" value="-"></span>
+            <span class="num"><input type ="submit" value="4"></span>
+            <span class="num"><input type ="submit" value="5"></span>
+            <span class="num"><input type ="submit" value="6"></span>
+            <span class="num plus"><input type ="submit" value="+"></span>
+            <span class="num"><input type ="submit" value="1"></span>
+            <span class="num"><input type ="submit" value="2"></span>
+            <span class="num"><input type ="submit" value="3"></span>
+            <span class="num"><input type ="submit" value="0"></span>
+            <span class="num"><input type ="submit" value="00"></span>
+            <span class="num"><input type ="submit" value="."></span>
+            <span class="num equal"><input type ="submit" value="="></span>
+        </form>
+    </div>
+</body>
---
+++ activitats/DAW-MP07/DAW-MP07-UF1/php-formularis-calc/readme.md
@@ -0,0 +1,28 @@
+# PHP - Calculadora (formulari)
+## DAW-MP07-UF1 - Exercici de Desenvolupament web en entorn servidor.
+En aquest exercici programarem una calculadora amb **HTML**, **CSS** i **PHP**. Caldrà programar les diferents funcionalitats d'aquesta.
+
+>Atenció: Aquesta solució és purament acadèmica. En aplicacions professionals no tindria sentit resoldre el problema proposat a la banda del servidor.
+
+Caldrà utilitzar el mètode **$_POST** per a recollir i processar les dades. Cal parar especial atenció a la llegibilitat del codi i usar funcions sempre que sigui possible.
+
+![Calculadora](im/calc.png)
+
+Utilitza els fitxers [index.php](index.php) i [style.css](style.css) com a base per al teu treball. Implementa la teva solució al fitxer **index.php**.
+Segurament caldrà modificar el codi **HTML** per a rebre la informació correctament.
+
+Assegura't que la calculadora sempre funcioni, mostra **ERROR** en la sintaxi impossible i **INF** quan toqui.
+
+### Un cop fet
+
+Afegeix els botons de parèntesi **( )**, **SIN** i **COS**. Implementa la seva funcionalitat.
+
+![Calculadora Retall](im/calc_extra.png)
+
+### A més a més (Extra)
+
+Fent servir expressions regulars, evita errors de l'usuari dotant de certa intel·ligència la calculadora. Per exemple: tanca els parèntesis quan sigui necessari.
+
+---
+###### Autor: Aniol Lidon 2022.09.27
+###### [CC BY](https://creativecommons.org/licenses/by/4.0/) ![CC BY](https://licensebuttons.net/l/by/3.0/80x15.png)
