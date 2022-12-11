ae650604a005b7598f69e6a4911954cb418c13c5
Aniol Lidon <alidon1@xtec.cat>
2022-12-11 23:09:33 +0100

    Adding plugin template

---
+++ activitats/DAW-MP07/DAW-MP07-UF3/wordpress-php-plugin/facelog/upload.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../../../wp-load.php';
+
+/** Sets up the WordPress Environment. */
+define('WP_USE_THEMES', false); /* Disable WP theme for this file (optional) */
